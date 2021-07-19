d5e1f9b7d01312929b538472f7910dacd3623b83
venuvannela <venuvannela1234@msitprogram.net>
2021-07-19 13:15:15 +0530

    remrowc

--- removeRowAndCol.py
+++ removeRowAndCol.py
@@ -23,6 +23,13 @@
 
 def removeRowAndCol(L, row, col):
     # Your code goes here...
-    pass
+    for i in L:
+        del i[col]
+    del L[row]    
+    return L
+    
+print(removeRowAndCol([ [ 2, 3, 4, 5], [ 8, 7, 6, 5], [ 0, 1, 2, 3] ], 1, 2))
+print(removeRowAndCol([ [ 5, 3, 6], [ 7, 2, 5], [ 1, 3, 4] ], 2, 2))
+print(removeRowAndCol([ [ 3, 4, 5, 6, 7], [ 8, 1, 2, 5, 9], [ 1, 4, 6, 9, 5] ], 1, 3))
 
 # Write your own test cases.
