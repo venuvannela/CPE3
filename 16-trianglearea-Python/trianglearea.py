c9e951214b848a52dd96a2fe691dbb0497d99e76
venuvannela <venuvannela1234@msitprogram.net>
2021-07-19 12:04:49 +0530

    are

--- 16-trianglearea-Python/trianglearea.py
+++ 16-trianglearea-Python/trianglearea.py
@@ -6,4 +6,13 @@
 
 def trianglearea(s1, s2, s3):
 	# your code goes here
-	pass
+	if(s1 + s2 <= s3) or (s1 + s3 <= s2) or (s2 + s3 <= s1):
+		
+		return 0
+
+	else:
+		x = (s1+s2+s3)/2
+		area = ((x*(x-s1)*(x-s2)*(x-s3))**0.5)
+		return area
+
+
