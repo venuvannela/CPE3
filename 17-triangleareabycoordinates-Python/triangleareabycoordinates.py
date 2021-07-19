0bec6e722c2ff1926bf1569b8b2d7c7d21ccfd4b
venuvannela <venuvannela1234@msitprogram.net>
2021-07-19 12:55:18 +0530

    trico

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -4,6 +4,17 @@
 # area of the triangle formed by those three points. Hint: you should make constructive use of
 # the triangleArea function you just wrote above.
 
+def trianglearea(s1, s2, s3):
+	if(s1 + s2 <= s3) or (s1 + s3 <= s2) or (s2 + s3 <= s1):
+		return 0
+	else:
+		x = (s1+s2+s3)/2
+		area = ((x*(x-s1)*(x-s2)*(x-s3))**0.5)
+		return area
+
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	s1 = ((((x2 - x1)**2) + ((y2 - y1)**2))**0.5)
+	s2 = ((((x3 - x2)**2) + ((y3 - y2)**2))**0.5)
+	s3 = ((((x1 - x3)**2) + ((y1 - y3)**2))**0.5)
+	return trianglearea(s1, s2, s3)
