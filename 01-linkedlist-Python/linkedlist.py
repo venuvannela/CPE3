cc6d4bf1bb784c8d2694f7b409bade2b44d2421d
venuvannela <venuvannela1234@msitprogram.net>
2021-08-02 10:11:55 +0530

    done

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -19,14 +19,30 @@ class LinkedList(object):
         
     def append(self, new_element):
         # Your code goes here
-        pass
-            
+        temp=self.head
+        if self.head:
+            while temp.next:
+                temp=temp.next
+            temp.next=new_element
+        else:
+            self.head=new_element
+
     def get_position(self, position):
         """Get an element from a particular position.
         Assume the first position is "1".
         Return "None" if position is not in the list."""
         # Your code goes here
-        pass
+        count=1
+        temp=self.head
+        if(position<1):
+            return None
+        while (temp and count<=position):
+            if (count==position):
+                return temp
+            temp=temp.next
+            count+= 1
+        return None
+
     
     def insert(self, new_element, position):
         """Insert a new node at the given position.
@@ -34,10 +50,31 @@ class LinkedList(object):
         Inserting at position 3 means between
         the 2nd and 3rd elements."""
         # Your code goes here
-        pass
+        count=1
+        temp=self.head
+        if(position>1):
+            while(temp and count<position):
+                if (count==position-1):
+                    new_element.next=temp.next
+                    temp.next=new_element
+                temp=temp.next
+                count+=1
+        elif (position==1):
+            new_element.next=self.head
+            self.head=new_element
+
     
     
     def delete(self, value):
         """Delete the first node with a given value."""
         # Your code goes here
-        pass
+        temp=self.head
+        previous=None
+        while(temp.value!=value and temp.next):
+            previous=temp
+            temp=temp.next
+        if (temp.value==value):
+            if(previous):
+                previous.next=temp.next
+            else:
+                self.head=temp.next
